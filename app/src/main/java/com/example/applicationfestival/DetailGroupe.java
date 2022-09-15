642a192115bf6441ac135c04b76bb4c2a2e3f313
Ewen CLÉMENT <ewenclement@gmail.com>
2022-09-15 19:18:24 +1100

    affichage image (utilisation librairie Glide) et arrangement visuel detail groupe

--- app/src/main/java/com/example/applicationfestival/DetailGroupe.java
+++ app/src/main/java/com/example/applicationfestival/DetailGroupe.java
@@ -2,8 +2,13 @@ package com.example.applicationfestival;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -12,19 +17,24 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DetailGroupe extends AppCompatActivity {
 
-    TextView nomArtiste;
-    TextView textePresentation;
-    TextView scene;
-    TextView jour;
-    TextView heure;
+    TextView nomArtisteTv;
+    ImageView imgGroupeIv;
+    TextView textePresentationTv;
+    TextView sceneTv;
+    TextView dateTv;
     String nomGroupe;
+    String urlImage;
+    String urlWebPage;
+    Button webPageBtn;
+    Button favoriBtn;
+    Context context;
 
     private RequestQueue mQueue;
 
@@ -32,12 +42,15 @@ public class DetailGroupe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_groupe);
+        context = getApplicationContext();
 
-        nomArtiste = findViewById(R.id.artiste);
-        textePresentation = findViewById(R.id.texte);
-        scene = findViewById(R.id.scene);
-        jour = findViewById(R.id.jour);
-        heure = findViewById(R.id.heure);
+        nomArtisteTv = findViewById(R.id.artiste);
+        imgGroupeIv = findViewById(R.id.imgGroupe);
+        textePresentationTv = findViewById(R.id.texte);
+        sceneTv = findViewById(R.id.scene);
+        dateTv = findViewById(R.id.date);
+        webPageBtn = findViewById(R.id.webPage);
+        favoriBtn = findViewById(R.id.favori);
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
@@ -46,11 +59,12 @@ public class DetailGroupe extends AppCompatActivity {
 
         // Initializing a new request queue
         mQueue = Volley.newRequestQueue(this);
+        // Glide.with(this).load(urlImage).into(imgGroupe);
         // Perform a GET request to parse the JSON
-        jsonParse();
+        jsonParse(context);
     }
 
-    private void jsonParse() {
+    private void jsonParse(Context context) {
         // URL API of a specific group
         String url = "https://daviddurand.info/D228/festival/info/"+nomGroupe;
 
@@ -64,11 +78,21 @@ public class DetailGroupe extends AppCompatActivity {
                             // we get the data object of the json object response
                             JSONObject infosGroupe = response.getJSONObject("data");
                             // we set the value of the TextViews with the corresponding key
-                            nomArtiste.setText(infosGroupe.getString("artiste"));
-                            textePresentation.setText(infosGroupe.getString("texte"));
-                            scene.setText(infosGroupe.getString("scene"));
-                            jour.setText(infosGroupe.getString("jour"));
-                            heure.setText(infosGroupe.getString("heure"));
+                            nomArtisteTv.setText(infosGroupe.getString("artiste"));
+                            // url with the image of the band
+                            urlImage = "https://daviddurand.info/D228/festival/illustrations/"+nomGroupe+"/image.jpg";
+                            // display the image of the band with url previously reached
+                            Glide.with(context).load(urlImage).into(imgGroupeIv);
+                            // we set the TextView for the info about the band
+                            textePresentationTv.setText(infosGroupe.getString("texte"));
+                            // info about the scene
+                            sceneTv.setText("Scène : " + infosGroupe.getString("scene"));
+                            // we set the TextView for the date of the concert
+                            dateTv.setText("Le "+ infosGroupe.getString("jour") + " à " + infosGroupe.getString("heure"));
+                            // we get the url of the webpage of the band
+                            urlWebPage = infosGroupe.getString("web");
+                            // we make the button clickable after we get the url web page
+                            webPageBtn.setClickable(true);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -82,5 +106,11 @@ public class DetailGroupe extends AppCompatActivity {
 
         // we add the request to the request queue
         mQueue.add(request);
+    }
+
+    public void goToWebPage(View view) {
+        Uri webpage = Uri.parse(urlWebPage);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
     }
 }
